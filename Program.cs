29ab3e3a6d119e8f20845af3c86931dea89e8513
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-11 22:25:13 -0300

    Realização da atividade

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome, sobrenome;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!; // Insere o que o usuário digitou na variável nome
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!; // Insere o que o usuário digitou na variável sobrenome
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}"); // Exibe a o conteúdo das variáveis 'nome' e 'sobrenome'
+
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}"); // Transforma o conteúdo da variável 'sobrenome' em maiúsculas, e exibe, seguido do conteúdo da variável 'nome'
